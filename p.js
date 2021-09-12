213500f8923740b4075629879c4e513afbc5097b
pratik2912 <pratiksurya2912@gmail.com>
2021-09-12 18:02:01 +0530

    first commit

---
+++ p.js
@@ -0,0 +1,18 @@
+alert('Please like the code and leave a comment')
+alert('Created by Pratik')
+var infor = " Your battery need a full charge 100%. Use cable 🔌 for charging your battery. A high bulk-charging rate wilk heat the battery, increase grassing and increase the absorption time needed to fully charge the battery.";
+var body = document.getElementById("info");
+setInterval(add,100);
+var i = 0;
+
+function add()
+{
+if(infor[i] == "Z"){
+    info.innerHTML += "\n";
+    i++;
+}
+if(i < infor.length){
+info.innerHTML+=infor[i];
+i++;
+""}
+}
